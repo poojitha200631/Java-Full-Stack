7a8b9eefa78233750bd19fba1386ecf1606896d4
poojitha200631 <poojitha200631@gmail.com>
2026-06-12 12:28:56 +0530

    Java.js

---
+++ HTML-CSS/manupulation.js
@@ -0,0 +1,15 @@
+let pen = document.getElementById("wish");
+console.log(pen);
+let value1=document.getElementsByClassName("wishes");
+console.log(value1);
+let book= document.getElementsByTagName("div");
+console.log(book);
+let table = document.querySelector("content");
+console.log(table);
+let phone = document.querySelectorAll("p");
+console.log(phone);
+let value2 = document.getElementById("wish");
+value2.innerHTML="<h1><u>HAPPY BIRTHDAY</u></h1>"
+value2.innerText="HAPPY BIRTHDAY TO ME";
+value2.style.fontsize="30px";
+value2.style.fontStyle="italic";
